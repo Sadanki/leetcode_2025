efa1cedbff79e5e5b593498fb5cf67eaf9ca0768
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2025-04-20 16:24:19 +0530

    Update 0169-majority-element.java

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -7,7 +7,7 @@ class Solution {
         
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1); // ✅ fix here
+                map.put(nums[i], map.get(nums[i]) + 1); 
             } else {
                 map.put(nums[i], 1);
             }
