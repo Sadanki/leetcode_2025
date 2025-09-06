638d82e475f5182f618d9d20dcad3fbce949c8e6
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2025-09-06 22:07:53 +0530

    Time: 29 ms (80.61%), Space: 42.7 MB (76.18%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,19 +1,25 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-      int sum=0;
-      for( int num: nums) sum+=num;
+        int sum = 0;
+        for (int num : nums) sum += num;
 
-      if(sum%2 !=0) return false;
+        // Step 1: If total sum is odd, we can't partition
+        if (sum % 2 != 0) return false;
 
-    int target =sum/2;
-    boolean[] dp = new boolean[target +1];
-    dp[0]= true;
+        int target = sum / 2;
 
-    for(int num: nums){
-        for(int i = target; i>=num; i--){
-            dp[i]= dp[i]||dp[i-num];
+        // Step 2: dp[i] means "can we make sum i from some subset of nums"
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true; // we can always make 0 sum with empty subset
+
+        // Step 3: Process each number
+        for (int num : nums) {
+            // Go backwards so we don't reuse a number twice
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
         }
-    }
-    return dp[target]; 
+
+        return dp[target]; // Step 4: Did we form sum = target?
     }
 }
