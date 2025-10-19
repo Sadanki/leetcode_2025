f22b27ae375dd8f01695fdcb6f035082163f75a0
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2025-10-19 19:06:53 +0530

    Time: 0 ms (100%), Space: 43.2 MB (29.37%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -15,6 +15,27 @@ class Solution {
             }
         }
 
-        return left; // Position where target should be inserted
+        return left; // insertion point
     }
 }
+
+/*Safer Formula: mid = left + (right - left) / 2
+
+This avoids overflow because:
+- right - left is guaranteed to be within bounds.
+- You’re adding a smaller number to left.
+\U0001f522 Example:
+Let’s say:
+left = 2_000_000_000
+right = 2_147_483_647
+
+
+- 
+
+- (left + right) / 2 → overflow
+- left + (right - left) / 2 → safe:
+- right - left = 147_483_647
+- mid = 2_000_000_000 + 73_741_823 = 2_073_741_823
+No overflow
+*/
+
