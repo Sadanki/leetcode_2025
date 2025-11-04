78b0d500c91d30f02ef741486b242eef9119deb8
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2025-11-04 17:32:08 +0530

    Time: 11 ms (56.71%), Space: 46.4 MB (12.02%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int calculate(String s) {
+        int res = 0;
+        int sign = 1;
+        int num = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0'); // build multi-digit number
+            } else if (c == '+') {
+                res += sign * num;
+                num = 0;
+                sign = 1;
+            } else if (c == '-') {
+                res += sign * num;
+                num = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(res);
+                stack.push(sign);
+                res = 0;
+                sign = 1;
+            } else if (c == ')') {
+                res += sign * num;
+                num = 0;
+                res *= stack.pop(); // sign before '('
+                res += stack.pop(); // result before '('
+            }
+        }
+
+        res += sign * num; // add any remaining number
+        return res;
+    }
+}
