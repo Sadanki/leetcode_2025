05b3eae257467e552b196ed36b3ba34af498dae8
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2025-11-04 17:39:34 +0530

    Time: 1 ms (59.64%), Space: 43.1 MB (7.06%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewelSet = new HashSet<>();
+        for (char c : jewels.toCharArray()) {
+            jewelSet.add(c); // store all jewel types
+        }
+
+        int count = 0;
+        for (char c : stones.toCharArray()) {
+            if (jewelSet.contains(c)) {
+                count++; // if stone is a jewel, increment
+            }
+        }
+
+        return count;
+    }
+}
