7e24af5ec7d8df2d207baa14a3384edf7f7a6f6d
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2025-11-25 05:16:28 +0530

    Time: 1 ms (82.24%), Space: 43.9 MB (11.39%) - LeetHub

---
+++ 2093-check-if-string-is-a-prefix-of-array/2093-check-if-string-is-a-prefix-of-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPrefixString(String s, String[] words) {
+        StringBuilder prefix = new StringBuilder();
+        
+        for (String w : words) {
+            prefix.append(w);
+            if (prefix.toString().equals(s)) {
+                return true;
+            }
+            if (prefix.length() > s.length()) {
+                return false;
+            }
+        }
+        
+        return false;
+    }
+}
