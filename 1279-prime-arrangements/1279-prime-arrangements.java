66956cf2d685d5ef3ab5a3cc55e87d980877444a
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2025-11-28 01:14:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (23.51%) - LeetHub

---
+++ 1279-prime-arrangements/1279-prime-arrangements.java
@@ -0,0 +1,35 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+    
+    public int numPrimeArrangements(int n) {
+        int primeCount = countPrimes(n);
+        int nonPrimeCount = n - primeCount;
+        
+        long result = (factorial(primeCount) * factorial(nonPrimeCount)) % MOD;
+        return (int) result;
+    }
+    
+    private int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) count++;
+        }
+        return count;
+    }
+    
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+    
+    private long factorial(int k) {
+        long res = 1;
+        for (int i = 2; i <= k; i++) {
+            res = (res * i) % MOD;
+        }
+        return res;
+    }
+}
