ae175897986ebd3a4aecf616c3e448b69a1d93e3
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2025-12-31 21:09:01 +0530

    Time: 24 ms (24.56%), Space: 76.6 MB (90.34%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // Sort the array
+        Arrays.sort(nums);
+        
+        // Check adjacent elements
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true; // Found a duplicate
+            }
+        }
+        
+        return false; // No duplicates
+    }
+}
