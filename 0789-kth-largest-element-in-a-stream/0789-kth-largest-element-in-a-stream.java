cadceadaba456194a826ecfef52c5da36235b8bd
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2026-01-02 04:52:02 +0530

    Time: 23 ms (65.62%), Space: 52.4 MB (24.28%) - LeetHub

---
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+
+class KthLargest {
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        minHeap = new PriorityQueue<>(k);
+        
+        // Initialize heap with nums
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        minHeap.offer(val);
+        if (minHeap.size() > k) {
+            minHeap.poll(); // remove smallest
+        }
+        return minHeap.peek(); // kth largest
+    }
+}
