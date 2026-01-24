45d6d7403b9ab23bb57e4b2fdaae89f9bd4d5aa0
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2026-01-25 01:39:02 +0530

    Time: 0 ms (100%), Space: 44.1 MB (52.36%) - LeetHub

---
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public bool LeafSimilar(TreeNode root1, TreeNode root2) {
+        var leaves1 = new List<int>();
+        var leaves2 = new List<int>();
+
+        GetLeaves(root1, leaves1);
+        GetLeaves(root2, leaves2);
+
+        if (leaves1.Count != leaves2.Count) return false;
+
+        for (int i = 0; i < leaves1.Count; i++) {
+            if (leaves1[i] != leaves2[i]) return false;
+        }
+
+        return true;
+    }
+
+    private void GetLeaves(TreeNode node, IList<int> leaves) {
+        if (node == null) return;
+
+        if (node.left == null && node.right == null) {
+            leaves.Add(node.val);
+            return;
+        }
+
+        GetLeaves(node.left, leaves);
+        GetLeaves(node.right, leaves);
+    }
+}
