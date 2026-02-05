37e8e664e16f1146440dcab4e0df7f8fa09196fc
Vignesh_Sadanake <165776478+Sadanki@users.noreply.github.com>
2026-02-06 05:05:47 +0530

    Time: 0 ms (100%), Space: 43 MB (98.33%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MinCostClimbingStairs(int[] cost) {
+        int n = cost.Length;
+        if (n == 1) return cost[0];
+        if (n == 2) return Math.Min(cost[0], cost[1]);
+
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.Min(dp[i - 1], dp[i - 2]);
+        }
+
+        return Math.Min(dp[n - 1], dp[n - 2]);
+    }
+}
